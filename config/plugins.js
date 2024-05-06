c0d86dc625cf74ce5d66ab310918727df209c12b
Marcel Pernia <marcelpm1987@gmail.com>
2024-05-06 23:46:34 +0200

    se reemplaza email provider por sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -20,14 +20,14 @@ module.exports = ({ env }) => ({
   },
   email: {
     config: {
-      provider: 'strapi-provider-email-resend',
+      provider: 'sendgrid',
       providerOptions: {
-        apiKey: env('RESEND_API_KEY'), // Required
+        apiKey: env('SENDGRID_API_KEY'),
       },
       settings: {
-        defaultFrom: 'onboarding@resend.dev',
+        defaultFrom: 'dev@tuiglesia.app',
         defaultReplyTo: 'dev@tuiglesia.app',
       },
-    }
+    },
   },
 });
