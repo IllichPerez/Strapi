6e27b9e71a828467a975da005b89b0ef37e94b68
Marcel Pernia <marcelpm1987@gmail.com>
2024-05-06 23:06:38 +0200

    Se configura proveedor de email Resend

--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'strapi-provider-email-resend',
+      providerOptions: {
+        apiKey: env('RESEND_API_KEY'), // Required
+      },
+      settings: {
+        defaultFrom: 'onboarding@resend.dev',
+        defaultReplyTo: 'dev@tuiglesia.app',
+      },
+    }
+  },
 });
