753d6b064249877353ae9f94159fbe16c4abee45
Marcel Pernia <marcelpm1987@gmail.com>
2025-07-23 16:04:50 +0200

    se comentan secciones de código en el ciclo de vida después de la actualización de suscripciones para facilitar la depuración

--- src/api/subscription/content-types/subscription/lifecycles.js
+++ src/api/subscription/content-types/subscription/lifecycles.js
@@ -1,50 +1,51 @@
 module.exports = {
   afterUpdate(event) {
     const { result } = event;
-    console.log(result)
-    const status = result.status;
-    const churchID = result.church.id;
+    console.log('Evento', event)
+    console.log('Resultado', result)
+    // const status = result.status;
+    // const churchID = result.church.id;
 
-    if (churchID && status === 'confirmed') {
-      const updateChurch = async () => {
-        // Calcular la fecha de expiración (un mes después de hoy)
-        const today = new Date();
-        const expirationDate = new Date(today);
+    // if (churchID && status === 'confirmed') {
+    //   const updateChurch = async () => {
+    //     // Calcular la fecha de expiración (un mes después de hoy)
+    //     const today = new Date();
+    //     const expirationDate = new Date(today);
         
-        // Agregar un mes
-        expirationDate.setMonth(expirationDate.getMonth() + 1);
+    //     // Agregar un mes
+    //     expirationDate.setMonth(expirationDate.getMonth() + 1);
         
-        // Si el día resultante no existe en el nuevo mes, ajustar al último día del mes
-        // Por ejemplo: 31 de enero → 28 de febrero (o 29 en año bisiesto)
-        const originalDay = today.getDate();
-        const newDay = expirationDate.getDate();
+    //     // Si el día resultante no existe en el nuevo mes, ajustar al último día del mes
+    //     // Por ejemplo: 31 de enero → 28 de febrero (o 29 en año bisiesto)
+    //     const originalDay = today.getDate();
+    //     const newDay = expirationDate.getDate();
         
-        if (originalDay !== newDay) {
-          // El día no existe en el nuevo mes, ajustar al último día del mes
-          expirationDate.setDate(0); // Esto establece la fecha al último día del mes anterior
-        }
+    //     if (originalDay !== newDay) {
+    //       // El día no existe en el nuevo mes, ajustar al último día del mes
+    //       expirationDate.setDate(0); // Esto establece la fecha al último día del mes anterior
+    //     }
         
-        const data = {
-          subscription_active: true,
-          subscription_expiration: expirationDate.toISOString().split('T')[0] // Formato YYYY-MM-DD
-        }
-        try {
-          const response = await fetch(`${process.env.URL}/api/churches/${churchID}`, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`
-            },
-            body: JSON.stringify({data})
-          })
-          const responseJson = await response.json()
-          console.log(responseJson)
-        } catch (error) {
-          console.error('Error creating post:', error)
-        }
-      }
-      updateChurch()
-    }
+    //     const data = {
+    //       subscription_active: true,
+    //       subscription_expiration: expirationDate.toISOString().split('T')[0] // Formato YYYY-MM-DD
+    //     }
+    //     try {
+    //       const response = await fetch(`${process.env.URL}/api/churches/${churchID}`, {
+    //         method: 'PUT',
+    //         headers: {
+    //           'Content-Type': 'application/json',
+    //           Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`
+    //         },
+    //         body: JSON.stringify({data})
+    //       })
+    //       const responseJson = await response.json()
+    //       console.log(responseJson)
+    //     } catch (error) {
+    //       console.error('Error creating post:', error)
+    //     }
+    //   }
+    //   updateChurch()
+    // }
 
   }
 }
